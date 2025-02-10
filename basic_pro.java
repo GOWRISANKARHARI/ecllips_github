1fab0f98094e5f0b61d6312e72fa8b071f69619e
Soundaranarayanan <141643132+Soundaranarayanan@users.noreply.github.com>
2025-02-10 13:15:51 +0530

    Update basic_pro.java

--- basic_pro.java
+++ basic_pro.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class basic_pro {
 	public static void main(String []args)
 	{
-		System.out.println("This is gowrisankar form KIOT");
+		System.out.println("This is gowrisankar from KIOT");
 	}
 }
